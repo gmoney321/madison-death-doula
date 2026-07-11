742eafca19cfc6fe5bca056f319c4fbe18cc18a6
gaven <gaven.desrosiers@gmail.com>
2026-07-11 17:16:23 -0500

    Made a mobile friendly hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const overlay = document.querySelector('.menu-overlay');
 const hamburger = document.querySelector('.hamburger');
+const closeButton = document.querySelector('.close-menu');
 
 hamburger.addEventListener('click', () => {
     overlay.classList.toggle('active');
+})
+
+closeButton.addEventListener('click', () => {
+    overlay.classList.remove('active');
 })
